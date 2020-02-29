09a13b18514fc8d39c711399a665b784d1948a34
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-02-29 09:18:14 -0500

    template

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db"
+);
 
 client.connect();
 
-const sync = async()=> {
-  //create tables and seed some data
-};
-
-//Additional methods here for reading, creating, destroying
-
+const sync = async () => {};
 
 module.exports = {
   sync
